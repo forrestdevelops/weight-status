46ac1aeab3117349d4b0c8d39dce6f6048ce854a
forrestdevelops <fsmietanski@gmail.com>
2024-01-16 20:52:45 -0500

    Removed redundant code for posts and removed unecessary procedures

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,5 +1,5 @@
-import { postRouter } from "~/server/api/routers/post";
 import { createTRPCRouter } from "~/server/api/trpc";
+import {weightRouter} from "~/server/api/routers/weight";
 
 /**
  * This is the primary router for your server.
@@ -7,7 +7,7 @@ import { createTRPCRouter } from "~/server/api/trpc";
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
-  post: postRouter,
+  weight: weightRouter,
 });
 
 // export type definition of API
