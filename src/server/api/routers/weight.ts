5ffe6dc9e917b02fb215c1509134f1c3d2616d64
forrestdevelops <fsmietanski@gmail.com>
2024-01-16 22:14:00 -0500

    Added Style to the nav bar

--- src/server/api/routers/weight.ts
+++ src/server/api/routers/weight.ts
@@ -10,8 +10,22 @@ export const weightRouter = createTRPCRouter({
         };
     }),
 
+    create: protectedProcedure
+        .input(z.object({ weight: z.number().min(100).max(500) }))
+        .mutation(async ({ ctx, input }) => {
+            // simulate a slow db call
+          //  await new Promise((resolve) => setTimeout(resolve, 1000));
+
+            // eslint-disable-next-line @typescript-eslint/no-unsafe-return,@typescript-eslint/no-unsafe-call,@typescript-eslint/no-unsafe-member-access
+            return ctx.db.weight.create({
+                data: {
+                    weight: input.weight,
+                    createdBy: { connect: { id: ctx.session.user.id } },
+                },
+            });
+        }),
 
     getProtectedMessage: protectedProcedure.query(() => {
-        return "This message is protected!!";
+        return "Welcome, ";
     }),
 });
